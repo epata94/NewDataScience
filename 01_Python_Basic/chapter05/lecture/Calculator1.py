711976ed4b0eda2b1c726b98c49baeb1caf60959
epata <hklee0724@gmail.com>
2022-11-13 23:32:38 +0900

    추가 파일

---
+++ 01_Python_Basic/chapter05/lecture/Calculator1.py
@@ -0,0 +1,6 @@
+first_num = int(input("첫번째 수를 입력하세요: "))
+second_num = int(input("두번째 수를 입력하세요: "))
+
+result = first_num + second_num
+
+print(f'{first_num} + {second_num} = {result}')
---
+++ 01_Python_Basic/chapter05/lecture/Calculator2.py
@@ -0,0 +1,46 @@
+# 리턴타입: X, 매개변수: X
+def add_number():
+    first_num = int(input("첫번째 수를 입력하세요: "))
+    second_num = int(input("두번째 수를 입력하세요: "))
+
+    result = first_num + second_num
+
+    print(f'{first_num} + {second_num} = {result}')
+
+add_number()
+
+#######################################################
+# 리턴타입: X, 매개변수: O
+def add_number2(first_num, second_num):
+
+    result = first_num + second_num
+
+    print(f'{first_num} + {second_num} = {result}')
+
+
+first_num = int(input("첫번째 수를 입력하세요: "))
+second_num = int(input("두번째 수를 입력하세요: "))
+add_number2(first_num, second_num)
+
+#######################################################
+# 리턴타입: O, 매개변수: X
+def add_number3():
+    first_num = int(input("첫번째 수를 입력하세요: "))
+    second_num = int(input("두번째 수를 입력하세요: "))
+    result = first_num + second_num
+
+    print(f'{first_num} + {second_num} = ', end='')
+    return result
+
+print(add_number3())
+
+#######################################################
+# 리턴타입: O, 매개변수: O
+def add_number4(first_num, second_num):
+    result = first_num + second_num
+
+    return result
+
+first_num = int(input("첫번째 수를 입력하세요: "))
+second_num = int(input("두번째 수를 입력하세요: "))
+print(f'{first_num} + {second_num} = {add_number4(first_num,second_num)}')
