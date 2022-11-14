3f15b52760847c25bc7be726f6b8840c11a487f4
TJ <hklee0724@gmail.com>
2022-11-14 15:06:05 +0900

    11월 14일 마지막 작업

---
+++ 01_Python_Basic/chapter04/lecture/1_stack_example.py
@@ -0,0 +1,17 @@
+# coding: cp949
+
+link_stack=[]
+
+print(link_stack)
+link_stack.append("皋牢 拳搁 林家")
+print(link_stack)
+link_stack.append("惑前 炼雀 林家")
+print(link_stack)
+link_stack.append("惑前 搬力 林家")
+print(link_stack)
+
+link_stack.pop()
+print(link_stack)
+link_stack.pop()
+print(link_stack)
+
