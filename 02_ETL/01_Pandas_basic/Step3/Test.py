3c062179945d59d23ea62c179f49e243dbb0f388
TJ <hklee0724@gmail.com>
2022-11-24 18:18:48 +0900

    11월 15일 마지막 작업

--- 02_ETL/01_Pandas_basic/Step3/Test.py
+++ 02_ETL/01_Pandas_basic/Step3/Test.py
@@ -1,6 +1,18 @@
 import pandas as pd
+import types
 op = pd.read_csv('../data/series_dataframe.csv')
-print(op)
+op_not = op.loc[(op['Series'] != op['DataFrame'])]
+op_ser = op_not['Series'].dropna()
+op_ser_list = op_ser.tolist()
 
-var = pd.Series.__dict__
-pass
+count = 0
+for i in op_ser_list:
+    try:
+
+        if type(pd.Series.__dict__[i]) == types.FunctionType:
+            print(i, end=',  ')
+            count += 1
+            if count % 5 == 0:
+                print()
+    except Exception as e:
+        pass
