511c6e9bc75ce699cf71e6739883bca0dd9fda4a
epata <hklee0724@gmail.com>
2022-12-15 07:40:15 +0900

    ..

--- 02_ETL/03_Data_Format/4. XML/COVID_Info_Parsing1.py
+++ 02_ETL/03_Data_Format/4. XML/COVID_Info_Parsing1.py
@@ -1,42 +1,33 @@
 from xml.etree.ElementTree import parse
 import pandas as pd
 
+from xml.etree.ElementTree import parse
+import pandas as pd
+
 def covid_xml_to_df():
     tree = parse("12월14일_코로나_예방접종현황.xml")
-    note = tree.getroot()
-    student_list=[]
-    for parent in note.iter("student"):
+    response = tree.getroot()
+    # 한꺼 번에 메서드 체이닝 하지 않는 이유는 추후에 item 노드에서 자동완성을 하기 위함이다.
+    items = response.find('body').find('items')
+    item_list=[]
+    for item in items.iter("item"):
 
-        simple_row_list=[] # 행 데이터
-        name = parent.get("name")
-        sex = parent.get("sex")
-        age = parent.findtext("age")
-        major = parent.findtext("major")
+        simple_row_list=[]
+        simple_row_list.append(item.find('tpcd').text)
+        simple_row_list.append(item.find('firstCnt').text)
+        simple_row_list.append(item.find('secondCnt').text)
+        simple_row_list.append(item.find('thirdCnt').text)
+        simple_row_list.append(item.find('fourCnt').text)
+        simple_row_list.append(item.find('winCnt').text)
+        simple_row_list.append(item.find('vrate').text)
+        simple_row_list.append(item.find('wrate').text)
 
-        simple_row_list.append(name)
-        simple_row_list.append(sex)
-        simple_row_list.append(age)
-        simple_row_list.append(major)
+        item_list.append(simple_row_list)
 
-
-        pcl_node = parent.find('practicable_computer_languages')
-        if pcl_node:
-            for language in pcl_node.iter("language"):
-                all_data_row_list=simple_row_list.copy()
-                all_data_row_list.append(language.get('name'))
-                all_data_row_list.append(language.get('level'))
-                all_data_row_list.append(language.find('period').get('value'))
-                student_list.append(all_data_row_list)
-        else:
-            simple_row_list.append('N/A')
-            simple_row_list.append('N/A')
-            simple_row_list.append('N/A')
-            student_list.append(simple_row_list)
-
-    column_list = ['이름','성별','나이','전공','컴퓨터언어','레벨','사용기간']
-    return pd.DataFrame(student_list, columns=column_list)
-
+    column_list = ['tpcd','firstCnt','secondCnt','thirdCnt','fourCnt','winCnt','vrate','wrate']
+    return pd.DataFrame(item_list, columns=column_list)
 
 df = covid_xml_to_df()
 print(df)
-df.to_csv('covid_info_1213.csv', encoding='cp949', index=False)
+
+df.to_csv('12월14일_코로나_예방접종현황_v1.csv', encoding='utf-8', index=False)
