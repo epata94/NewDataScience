1237e50761660928e2e4c53483b0393aa8510f54
lodosswkor <lodossw@gmail.com>
2023-01-09 12:01:06 +0900

    ..

---
+++ 04_AI/ch02/Perceptron_Sample.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+class Perceptron(object):
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        # scale: 표준 편차 0.01, size: 특성 개수 보다 1개 크게 만든다. (절편 고려)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        """입력 계산"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        """단위 계단 함수를 사용하여 클래스 레이블을 반환합니다"""
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+import os
+import pandas as pd
+
+s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+print('URL:', s)
+
+df = pd.read_csv(s,
+                 header=None,
+                 encoding='utf-8')
+
+y = df.iloc[0:100, 4].values
+# setosa면 -1 아니면 1
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# 꽃받침 길이와 꽃잎 길이를 추출합니다
+X = df.iloc[0:100, [0, 2]].values
+
+pass
+
+p = Perceptron()
+p.net_input(X)
