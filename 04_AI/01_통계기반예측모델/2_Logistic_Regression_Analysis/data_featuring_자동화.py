0f91d205ed1cb39ae10f93196da1012ea05ed3a3
lodosswkor <lodossw@gmail.com>
2023-01-19 18:14:03 +0900

    ..

--- 04_AI/01_통계기반예측모델/2_Logistic_Regression_Analysis/data_featuring_자동화.py
+++ 04_AI/01_통계기반예측모델/2_Logistic_Regression_Analysis/data_featuring_자동화.py
@@ -4,7 +4,8 @@ import pandas as pd
 import statsmodels.api as sm
 from itertools import combinations
 import operator
-
+import warnings
+warnings.filterwarnings('ignore')
 from datetime import datetime
 import time
 
@@ -30,26 +31,29 @@ len_colums = len(colums_list)
 dependent_variable = churn['churn']
 
 start = datetime.fromtimestamp(time.time())
-for num in range(9,len_colums+1):
+for num in range(9, len_colums + 1):
 	combi_list = list(combinations(colums_list, num))
 
 	for tup in combi_list:
-		column_list_combi = list(tup)
-		independent_variables = churn[column_list_combi]
-		independent_variables_with_constant = sm.add_constant(independent_variables, prepend=True)
-		logit_model = sm.Logit(dependent_variable, independent_variables_with_constant).fit()
-		match_count = 0
-		new_observatios = churn.loc[:, independent_variables.columns]
-		new_observatios_with_constant = sm.add_constant(new_observatios, prepend=True)
-		y_predicted = logit_model.predict(new_observatios_with_constant)
-		y_predicted_rounded = [round(score, 0) for score in y_predicted]
-		for index in range(len(y_predicted_rounded)):
-			if y_predicted_rounded[index] == dependent_variable.values[index]:
-				match_count += 1
-		print(f'\n독립변수 조합 >> {column_list_combi}')
-		print('>> match count=', match_count)
-		print('>> 정답률: %.2f %%' % (match_count / len(y_predicted_rounded) * 100))
-		match_dic[' '.join(column_list_combi)] = match_count / len(y_predicted_rounded) * 100
+		try:
+			column_list_combi = list(tup)
+			independent_variables = churn[column_list_combi]
+			independent_variables_with_constant = sm.add_constant(independent_variables, prepend=True)
+			logit_model = sm.Logit(dependent_variable, independent_variables_with_constant).fit()
+			match_count = 0
+			new_observatios = churn.loc[:, independent_variables.columns]
+			new_observatios_with_constant = sm.add_constant(new_observatios, prepend=True)
+			y_predicted = logit_model.predict(new_observatios_with_constant)
+			y_predicted_rounded = [round(score, 0) for score in y_predicted]
+			for index in range(len(y_predicted_rounded)):
+				if y_predicted_rounded[index] == dependent_variable.values[index]:
+					match_count += 1
+			print(f'\n독립변수 조합 >> {column_list_combi}')
+			print('>> match count=', match_count)
+			print('>> 정답률: %.2f %%' % (match_count / len(y_predicted_rounded) * 100))
+			match_dic[' '.join(column_list_combi)] = match_count / len(y_predicted_rounded) * 100
+		except:
+			pass
 
 # 정답률 최대값 찾기
 match_dic = sorted(match_dic.items(), key=operator.itemgetter(1),reverse=True)
@@ -59,10 +63,10 @@ print("\n\n 독립변수 최적화 분석 결과")
 print('총 조합 갯수: %d'%len(match_dic))
 print("MAX 조합: %s >> %.2f %%"%(match_dic[0][0],match_dic[0][1]))
 
-# end = datetime.fromtimestamp(time.time())
-# print(f'\n분석 시작: {start}')
-# print(f'분석 종료: {end}')
-# print(f'총 분석 시간: {end-start}')
+end = datetime.fromtimestamp(time.time())
+print(f'\n분석 시작: {start}')
+print(f'분석 종료: {end}')
+print(f'총 분석 시간: {end-start}')
 
 #  독립변수 최적화 분석 결과
 # 총 조합 갯수: 354522
