c09b3f1a27d445af09f6021fd898eee98c6814e5
lodosswkor <lodossw@gmail.com>
2023-01-30 18:17:48 +0900

    ..

--- 04_AI/04_응용/01_텍스트마이닝/2.1_단어빈도_분석_konlpy설치 및 동작확인.py
+++
@@ -1,8 +0,0 @@
-#py -m pip install JPype1-0.6.3-cp36-cp36m-win_amd64.whl
-#py -m pip install konlpy
-
-from konlpy.tag import Okt
-okt = Okt()
-malist = okt.pos("아버지가 방에 들어가신다.", norm=True, stem=True)
-# malist = okt.pos("아버지 가방에 들어가신다.", norm=True, stem=True)
-print(malist)
